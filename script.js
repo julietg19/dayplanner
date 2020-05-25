c3d29bf9816988950f8cf02b53bed3173ba6bffe
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-05-25 13:36:37 -0500

    colors

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 document.getElementById("date-today").innerHTML = moment().format(
   "MMMM Do YYYY, h:mm"
 );
+
+var currentHour = moment().format("h");
+
+for (i = 1; i <= 10; i++) {
+  rowTime = document.getElementById("time" + i).innerHTML;
+  // if currenttime is = rowtime : make row red
+  console.log(currentHour, rowTime);
+
+  if (moment().isSame(moment(rowTime, "h a"), "hour")) {
+    document.getElementById("text-entry" + 1).style.backgroundColor = "red";
+  }
+
+  // if currenttime is < row time : make row green
+
+  if (moment().isBefore(moment(rowTime, "h a"), "hour")) {
+    document.getElementById("text-entry" + i).style.backgroundColor = "green";
+  }
+
+  // if currenttime is > rowtime : make row grey
+
+  if (moment().isAfter(moment(rowTime, "h a"), "hour")) {
+    document.getElementById("text-entry" + i).style.backgroundColor = "grey";
+  }
+
+
+
+// rowTime = document.getElementById("time1").innerHTML;
+
+// moment(currentHour).isSame(rowTime, "hour");
